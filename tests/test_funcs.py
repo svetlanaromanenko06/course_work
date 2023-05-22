331a2b78d956e0166e3ed724753c1de51688ae26
svetlana romanenko <svroman07@gmail.com>
2023-05-23 01:50:01 +0300

    fix test

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -2,23 +2,7 @@ from utils import funcs
 
 
 def test_sort_by_date():
-    test_data = [
-        {
-            "id": 490100847,
-            "state": "EXECUTED",
-            "date": "2018-12-22T02:02:49.564873",
-            "operationAmount": {
-                "amount": "56516.63",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод с карты на карту",
-            "from": "Visa Gold 8326537236216459",
-            "to": "MasterCard 6783917276771847"
-        },
-        {
+    test_data = [{
             "id": 522357576,
             "state": "EXECUTED",
             "date": "2019-07-12T20:41:47.882230",
@@ -63,22 +47,7 @@ def test_sort_by_date():
             "from": "Счет 48894435694657014368",
             "to": "Счет 38976430693692818358"
         },
-        {
-            "id": 490100847,
-            "state": "EXECUTED",
-            "date": "2018-12-22T02:02:49.564873",
-            "operationAmount": {
-                "amount": "56516.63",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод с карты на карту",
-            "from": "Visa Gold 8326537236216459",
-            "to": "MasterCard 6783917276771847"
-        },
-        {
+             {
             "id": 596171168,
             "state": "EXECUTED",
             "date": "2018-07-11T02:26:18.671407",
